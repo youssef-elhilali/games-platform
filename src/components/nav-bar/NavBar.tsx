c62a72883d60bf835c49c3be326b47948e6c3eb3
youssef-elhilali <y.elhilali24@gmail.com>
2023-11-07 10:42:47 +0100

    Build color mode switch

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../assets/logo.jpg";
+import ColorModeSwitch from "../ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="15px">
       <Image borderRadius="full" boxSize="70px" src={logo} alt="Dan Abramov" />
+      <ColorModeSwitch />
     </HStack>
   );
 };
